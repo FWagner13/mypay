31544838426eeeed1b945a7ae047468a19d84d6f
Felipe Wagner <felipe.wagner@live.com>
2024-01-10 13:38:19 -0300

    fix: ajustes de annotations e metodos publicos da domain Account

--- src/main/java/com/mypay/domain/Account.java
+++ src/main/java/com/mypay/domain/Account.java
@@ -2,20 +2,29 @@ package com.mypay.domain;
 
 import java.math.BigDecimal;
 
+import com.mypay.account.AccountType;
+import com.mypay.dto.NewAccountDTO;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
@@ -25,8 +34,18 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne(mappedBy = "account")
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
     private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
+
+    public Account(AccountType type, User user) {
+        this.balance = new BigDecimal(0);
+        this.type = type;
+        this.user = user;
+    }
 }
